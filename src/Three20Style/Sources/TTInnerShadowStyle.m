2136957a1f1e2a33240f75a334d437a45f2e5137
Per Eckerdal <per.eckerdal@gmail.com>
2012-07-04 22:15:26 +0200

    Reduced visual glitch when using TTInnerShadowStyle together with TTShapeStyle and rounded corners against dark background

--- src/Three20Style/Sources/TTInnerShadowStyle.m
+++ src/Three20Style/Sources/TTInnerShadowStyle.m
@@ -45,7 +45,7 @@
   CGContextClip(ctx);
 
   [context.shape addInverseToPath:context.frame];
-  [[UIColor whiteColor] setFill];
+  [[_color colorWithAlphaComponent:1] setFill];
 
   // Due to a bug in OS versions 3.2 and 4.0, the shadow appears upside-down. It pains me to
   // write this, but a lot of research has failed to turn up a way to detect the flipped shadow
